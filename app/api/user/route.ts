2622e3d79e2cd2ff32836419b414a48920fece1d
Yamasaki Miyuu <y-miyu-2429@occ.co.jp>
2024-07-02 15:57:05 +0900

    add "export const dynamic = 'force-dynamic'"

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+export const dynamic = 'force-dynamic'
 
 export const GET = async (req: Request, res: NextResponse) => {
     const users = await prisma.user.findMany({
